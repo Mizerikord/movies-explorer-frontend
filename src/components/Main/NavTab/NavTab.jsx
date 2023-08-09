f85632639e39816aafc4fafee75d7fde6420bdeb
unknown <greyguard89@mail.ru>
2023-08-09 21:52:06 +0300

    fix anchor

--- src/components/Main/NavTab/NavTab.jsx
+++ src/components/Main/NavTab/NavTab.jsx
@@ -1,13 +1,13 @@
 
-import { Link } from "react-router-dom";
+import { HashLink as Link } from 'react-router-hash-link';
 import './navtab.css';
 
 function NavTab(props) {
     return (
         <ul className="nav-list">
-            <li className="nav-item link"><Link to='/#about' className="nav-link">О проекте</Link></li>
-            <li className="nav-item link"><Link to='/#tech' className="nav-link">Технологии</Link></li>
-            <li className="nav-item link"><Link to='/#student' className="nav-link">Студент</Link></li>
+            <li className="nav-item link"><Link to="/#about" className="nav-link">О проекте</Link></li>
+            <li className="nav-item link"><Link to="/#tech" className="nav-link">Технологии</Link></li>
+            <li className="nav-item link"><Link to="/#student" className="nav-link">Студент</Link></li>
         </ul>
     );
 }
